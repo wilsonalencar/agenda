b2f53adf8a7910f5742b9fa05abc30b6eb71df98
root <lucas.saboreweb@gmail.com>
2018-06-18 17:46:31 -0300

    resolução de bugs

--- app/Http/Controllers/GuiaicmsController.php
+++ app/Http/Controllers/GuiaicmsController.php
@@ -98,10 +98,8 @@ class GuiaicmsController extends Controller
                 $arr[$file]['path'] = substr($destino, 0, -9); 
                 $arr[$file]['arquivotxt'] = $arquivonome; 
                 $arr[$file]['pathtxt'] = substr($caminho1_result, 0, -8);
-                echo "<Pre>";
-                print_r($arr);exit;
                 copy($file, $destino);
-                //unlink($file);
+                // unlink($file);
             }
         }
         // if (!empty($files)) {
@@ -372,7 +370,8 @@ cnpj/cpf/insc. est.:([^{]*)~i', $str, $match);
         $str = utf8_encode($str);
         $str = preg_replace(array("/(á|à|ã|â|ä)/","/(Á|À|Ã|Â|Ä)/","/(é|è|ê|ë)/","/(É|È|Ê|Ë)/","/(í|ì|î|ï)/","/(Í|Ì|Î|Ï)/","/(ó|ò|õ|ô|ö)/","/(Ó|Ò|Õ|Ô|Ö)/","/(ú|ù|û|ü)/","/(Ú|Ù|Û|Ü)/","/(ñ)/","/(Ñ)/","/(ç)/","/(Ç)/","/(ª)/","/(°)/"),explode(" ","a A e E i I o O u U n N c C um um"),$str);
         $str = strtolower($str);
-
+        echo "<PRe>";
+        print_r($str);exit;
         $icms['TRIBUTO_ID'] = 8;
         
         //razão social

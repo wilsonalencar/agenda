09262385a188ac14503d46cf102d60b23e4d0b94
root <lucas.saboreweb@gmail.com>
2018-09-03 16:12:11 -0300

    Inserindo melhorias

--- app/Http/Controllers/GuiaicmsController.php
+++ app/Http/Controllers/GuiaicmsController.php
@@ -1177,14 +1177,18 @@ valor total([^{]*)~i', $str, $match);
            $icms['REFERENCIA'] = $k[0].'/'.$k[1];
         }
         
-        preg_match('~codigo numerico linha digitavel([^{]*)~i', $str, $match);
+        preg_match('~
+valor total
+
+([^{]*)~i', $str, $match);
         if (!empty($match)) {
             $i = explode('
 ', trim($match[1]));
-            $a = explode(' ', $i[0]);
-            $icms['VLR_RECEITA'] = str_replace(',', '.', str_replace('.', '', trim($a[0])));
-        }
-
+            $icms['VLR_RECEITA'] = str_replace(',', '.', str_replace('.', '', trim($i[2])));
+            $icms['VLR_TOTAL'] = str_replace(',', '.', str_replace('.', '', trim($i[2])));
+            $icms['CODBARRAS'] = trim(str_replace('observacao', '', trim(str_replace(' ', '', $i[0]))));
+       }
+        /*
         preg_match('~validade
 
 valor total([^{]*)~i', $str, $match);
@@ -1195,7 +1199,6 @@ valor total([^{]*)~i', $str, $match);
             $codbarras = str_replace('-', '', str_replace(' ', '', $i[4]));
             $icms['CODBARRAS'] = trim($codbarras);
         }
-
         preg_match('~observacao([^{]*)~i', $str, $match);
         if (!empty($match)) {
             $i = explode('
@@ -1209,6 +1212,7 @@ valor total([^{]*)~i', $str, $match);
                 $icms['OBSERVACAO'] .= $value.' ';
             }
         }
+        */
         
         fclose($handle);
         $icmsarray = array();
@@ -2336,6 +2340,7 @@ valor total([^{]*)~i', $str, $match);
             
             $icms['VLR_RECEITA'] = str_replace(',', '.', str_replace('.', '',trim($a[1])));
             if(isset($a[1]) && strlen($a[1]) == 1){
+                // $icms['VLR_RECEITA'] = str_replace(',', '.', str_replace('.', '',trim($a[2])));             
                 $icms['VLR_RECEITA'] = $a[1].str_replace(',', '.', str_replace('.', '',trim($a[2])));             
             }
 
@@ -2343,6 +2348,7 @@ valor total([^{]*)~i', $str, $match);
             $icms['MULTA_MORA_INFRA'] = str_replace(',', '.', str_replace('.', '',trim($a[5])));
             $icms['VLR_TOTAL'] = str_replace(',', '.', str_replace('.', '',trim($a[6])));
             if(isset($a[7]) && strlen($a[7]) == 1){
+                // $icms['VLR_TOTAL'] = str_replace(',', '.', str_replace('.', '',trim($a[8])));
                 $icms['VLR_TOTAL'] = $a[7].str_replace(',', '.', str_replace('.', '',trim($a[8])));
             }
         }

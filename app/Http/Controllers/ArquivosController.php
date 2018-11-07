5e19f390cadce0461002dd4b57582260b854d632
Wilson Junior <wilsonalencar.junior@gmail.com>
2018-11-07 16:13:24 -0200

    correção

--- app/Http/Controllers/ArquivosController.php
+++ app/Http/Controllers/ArquivosController.php
@@ -107,6 +107,8 @@ class ArquivosController extends Controller
                 $singlefilename = explode('/', $file);
                 foreach ($singlefilename as $xx => $v) {
                 }
+
+                $v = $v.'_'.$index;
                 if (file_exists($file)) {
                     $zip->addFile($file, $v);
                 }

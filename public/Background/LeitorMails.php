fbdc6413529e1e63b39cf3ca02e1b22fbc2589d6
Wilson Junior <wilsonalencar.junior@gmail.com>
2018-12-03 11:35:13 -0200

    correção

---
+++ public/Background/LeitorMails.php
@@ -0,0 +1,8 @@
+<?php
+$ch = curl_init();
+$link = $_SERVER['HTTP_HOST'].'/leitor/job';
+curl_setopt($ch, CURLOPT_URL, $link);
+curl_setopt($ch, CURLOPT_HEADER, 0);
+curl_exec($ch);
+curl_close($ch);
+?>

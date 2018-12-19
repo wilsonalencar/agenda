80f5b8a61aa0a0abaf9c70fc67283061320c1ed4
Wilson Junior <wilsonalencar.junior@gmail.com>
2018-12-18 23:22:57 -0200

    correção

--- app/Http/Controllers/UploadsController.php
+++ app/Http/Controllers/UploadsController.php
@@ -56,7 +56,7 @@ class UploadsController extends Controller
         
         if (!empty($atividades)) {
             foreach ($atividades as $key => $atividade) {
-                if (strtotime($atividade->limite) < strtotime($atividade->data_atividade)) {
+                if (strtotime(substr($atividade->limite, 0,10)) < strtotime(substr($atividade->data_atividade, 0,10))) {
                     $atividadesFiltered[$atividade->tributo][$atividade->uf][$atividade->status]['PrazoEstourado'][] = $atividade;
                 } else {
                     $atividadesFiltered[$atividade->tributo][$atividade->uf][$atividade->status]['Prazo'][] = $atividade;
